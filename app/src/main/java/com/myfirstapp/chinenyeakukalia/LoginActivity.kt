c2ddd08703525ae5a029439a6810f57ae7636312
Chinenye Akukalia <sonya12slyvia@gmail.com>
2023-11-26 18:53:40 -0800

    MainActivity LoginActivity CreateAccountActivity ForgotPasswordActivity ResetActivity

--- app/src/main/java/com/myfirstapp/chinenyeakukalia/LoginActivity.kt
+++ app/src/main/java/com/myfirstapp/chinenyeakukalia/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.myfirstapp.chinenyeakukalia
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +29,17 @@ class LoginActivity : AppCompatActivity() {
         }
         val loginButton: Button = findViewById(R.id.login)
         loginButton.setOnClickListener {
-
-            val i = Intent(this, HomeActivity::class.java)
-
-            startActivity(i)
+            val inputField: EditText = findViewById(R.id.editusername)
+            val passField: EditText = findViewById(R.id.password)
+            val userInputEnter: String = inputField.text.toString()
+            val userPassEnter: String = passField.text.toString()
+            var message = "You need to enter Username and Password"
+            if (userInputEnter.isEmpty() || userPassEnter.isEmpty()) {
+                Snackbar.make(findViewById(R.id.parentLogin), message, Snackbar.LENGTH_LONG).show()
+            }else {
+                val l = Intent(this, HomeActivity::class.java)
+                startActivity(l)
+            }
         }
     }
 }
--- app/src/main/java/com/myfirstapp/chinenyeakukalia/ResetActivity.kt
+++ app/src/main/java/com/myfirstapp/chinenyeakukalia/ResetActivity.kt
@@ -3,7 +3,9 @@ package com.myfirstapp.chinenyeakukalia
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class ResetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +18,23 @@ class ResetActivity : AppCompatActivity() {
 
             startActivity(i)
         }
-        val backButton: Button = findViewById(R.id.back)
-        backButton.setOnClickListener {
+        val restButton: Button = findViewById(R.id.resetPassword)
+        resetButton.setOnClickListener {
+            val enterCodeInput: EditText = findViewById(R.id.enterCode)
+            val enterNewPassInput: EditText = findViewById(R.id.newPass)
+            val reTypeInput: EditText = findViewById(R.id.retypePass)
+            val userCodeEnter: String = enterCodeInput.text.toString()
+            val userNewPassEnter: String = enterNewPassInput.text.toString()
+            val reTypePassEnter: String = reTypeInput.text.toString()
+            var message = "You need to enter code and new password and retype password"
+            if (userCodeEnter.isEmpty() || userNewPassEnter.isEmpty() || reTypePassEnter.isEmpty()) {
+                Snackbar.make(findViewById(R.id.parentReset), message, Snackbar.LENGTH_LONG).show()
+            } else {
 
-            val i = Intent(this, LoginActivity::class.java)
+                val i = Intent(this, LoginActivity::class.java)
 
-            startActivity(i)
+                startActivity(i)
+            }
         }
     }
 }
